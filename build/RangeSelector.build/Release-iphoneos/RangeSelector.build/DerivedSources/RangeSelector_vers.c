91e4e3d1d09effb489547f90f6c5b6f49bd50775
Carl Goldsmith <me@carlgoldsmith.com>
2017-03-20 08:26:32 +0000

    Removed build folder

--- build/RangeSelector.build/Release-iphoneos/RangeSelector.build/DerivedSources/RangeSelector_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char RangeSelectorVersionString[];
- extern const double RangeSelectorVersionNumber;
-
- const unsigned char RangeSelectorVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RangeSelector  PROJECT:RangeSelector-1" "\n";
- const double RangeSelectorVersionNumber __attribute__ ((used)) = (double)1.;
--- build/RangeSelector.build/Release-iphoneos/RangeSelector.build/Objects-normal/arm64/RangeSelector_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/carl/Developer/Pods/RangeSelector/build/RangeSelector.build/Release-iphoneos/RangeSelector.build/DerivedSources/RangeSelector_vers.c
